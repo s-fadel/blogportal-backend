c11a9186dd35cf700490022e808233957cd7a7a3
aylinataei <aylin.ataei@hotmail.com>
2023-10-26 14:54:00 +0200

    working backend

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+
 const app = express();
 const db = require("./app/models");
+
 const Role = db.role;
 
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   console.log('Drop and Resync Db');
   initial();
 });
@@ -16,12 +18,12 @@ function initial() {
     id: 1,
     name: "user"
   });
- 
+
   Role.create({
     id: 2,
     name: "moderator"
   });
- 
+
   Role.create({
     id: 3,
     name: "admin"
@@ -44,6 +46,11 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
